f94994ef385c6c33172620c3519dac282590f045
dias002 <dias.sun666@gmail.com>
2026-02-28 13:12:28 +0500

    budget model

--- djangoApi/finance/admin.py
+++ djangoApi/finance/admin.py
@@ -5,3 +5,8 @@ from .models import Category, Budget, Transaction
 admin.site.register(Category)
 admin.site.register(Budget)
 admin.site.register(Transaction)
+
+
+# git add . 
+# git commit -m "made header and other staff"
+# git push
